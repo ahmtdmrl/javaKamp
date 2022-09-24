69ddd29a1f88214666221eb1c5a38a00026a0021
ahmtdmrl <50130966+ahmtdmrl@users.noreply.github.com>
2022-09-24 18:27:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    //camelCase
+    public static void main(String[] args) {
+        int ogrenciSayısı=12;
+        String mesaj = "Ogrenci sayısı :";
+        System.out.println (mesaj + ogrenciSayısı);
+        System.out.println("ögrenci sayısı = " + ogrenciSayısı);
+        System.out.println("ögrenci sayısı = " + ogrenciSayısı);
+        System.out.println("ögrenci sayısı = " + ogrenciSayısı);
+        System.out.println("ögrenci sayısı = " + ogrenciSayısı);
+
+
+
+    }
+}
